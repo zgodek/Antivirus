eea1e0fbb1016ae95823da9253603ca1b421b8f4
zgodek <zuzanna.godek@outlook.com>
2021-12-26 18:36:08 +0100

    Checks if path leads to a file

--- antivirus.py
+++ antivirus.py
@@ -1,12 +1,16 @@
 import hashlib
+import os
 
 
 def hash_a_file(path):
-    BLOCKSIZE = 65536
-    hasher = hashlib.sha1()
-    with open(path, 'rb') as afile:
-        buf = afile.read(BLOCKSIZE)
-        while len(buf) > 0:
-            hasher.update(buf)
+    if os.path.isfile(path):
+        BLOCKSIZE = 65536
+        hasher = hashlib.sha1()
+        with open(path, 'rb') as afile:
             buf = afile.read(BLOCKSIZE)
-    return(hasher.hexdigest())
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = afile.read(BLOCKSIZE)
+        return hasher.hexdigest()
+    else:
+        return None
