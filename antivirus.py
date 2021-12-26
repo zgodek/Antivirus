5a09d0c04fc1673ac77c256bcff116f19cb31027
zgodek <zuzanna.godek@outlook.com>
2021-12-26 19:03:41 +0100

    Added hash_a_folder function.

--- antivirus.py
+++ antivirus.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+from pathlib import Path
 
 
 def hash_a_file(path):
@@ -14,3 +15,21 @@ def hash_a_file(path):
         return hasher.hexdigest()
     else:
         return None
+
+
+def hash_a_folder(path):
+    p = Path(path)
+    for x in p.iterdir():
+        if x.is_dir():
+            hash_a_folder(x)
+        elif x.is_file():
+            BLOCKSIZE = 65536
+            hasher = hashlib.sha1()
+            with open(x, 'rb') as afile:
+                buf = afile.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile.read(BLOCKSIZE)
+            print(hasher.hexdigest())
+        else:
+            return('Incorrect path.')
