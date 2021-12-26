d9e3d2834d984baacd1a88221287435f7ce117b7
zgodek <zuzanna.godek@outlook.com>
2021-12-26 20:35:50 +0100

    Implemented create_an_index instead of hash_a_folder.

--- antivirus.py
+++ antivirus.py
@@ -1,35 +1,44 @@
 import hashlib
-import os
 from pathlib import Path
 
 
+class File:
+    def __init__(self, path):
+        self._path = path
+        self._hash = hash_a_file(self.path())
+
+    def path(self):
+        return self._path
+
+    def satus(self):
+        pass
+
+    def hash(self):
+        return self._hash
+
+
 def hash_a_file(path):
-    if os.path.isfile(path):
-        BLOCKSIZE = 65536
-        hasher = hashlib.sha1()
-        with open(path, 'rb') as afile:
+    BLOCKSIZE = 65536
+    hasher = hashlib.sha1()
+    with open(path, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
             buf = afile.read(BLOCKSIZE)
-            while len(buf) > 0:
-                hasher.update(buf)
-                buf = afile.read(BLOCKSIZE)
-        return hasher.hexdigest()
-    else:
-        return None
+    return hasher.hexdigest()
 
 
-def hash_a_folder(path):
+def create_an_index(path):
+    index = []
     p = Path(path)
     for x in p.iterdir():
         if x.is_dir():
-            hash_a_folder(x)
+            for file in create_an_index(x):
+                index.append(file)
         elif x.is_file():
-            BLOCKSIZE = 65536
-            hasher = hashlib.sha1()
-            with open(x, 'rb') as afile:
-                buf = afile.read(BLOCKSIZE)
-                while len(buf) > 0:
-                    hasher.update(buf)
-                    buf = afile.read(BLOCKSIZE)
-            print(hasher.hexdigest())
-        else:
-            return('Incorrect path.')
+            index.append(File(x))
+    return index
+
+
+for file in create_an_index("/home/zgodek/pipr/antivirus/test_dir"):
+    print(file.path(), file.hash())
