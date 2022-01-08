71dec99c0e25c6cd9c2010acf56109ca5b5d03a0
zgodek <zuzanna.godek@outlook.com>
2022-01-08 18:52:27 +0100

    Split one function into two: one reads database with hashes, the other one reads database with code sequences.

--- database.py
+++ database.py
@@ -2,14 +2,23 @@ from pathlib import Path
 import json
 
 
-def read_virus_database(path):
+def read_virus_database_md5(path):
     path_database = Path(path)
-    list_of_items = []
+    list_of_hashes = []
     for item_path in path_database.iterdir():
         with open(item_path, 'r') as file_handle:
             for line in file_handle:
-                list_of_items.append(str(line).rstrip())
-    return list_of_items
+                list_of_hashes.append(str(line).rstrip())
+    return list_of_hashes
+
+
+def read_virus_database_sequences(path):
+    path_database = Path(path)
+    list_of_sequences = []
+    for item_path in path_database.iterdir():
+        with open(item_path, 'rb') as file_handle:
+            list_of_sequences.append(file_handle.read())
+    return list_of_sequences
 
 
 def read_index_database(path):
