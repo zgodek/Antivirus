cd890d5ef41a3eb020f46a10369f4f3d254e548d
zgodek <zuzanna.godek@outlook.com>
2022-01-08 10:13:52 +0100

    All functions related to managing index.

---
+++ index.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import json
+from file import File
+
+
+def create_a_list_of_files(path):
+    list_of_files = []
+    path = Path(path)
+    for item_path in path.iterdir():
+        if item_path.is_dir():
+            for file in create_a_list_of_files(item_path):
+                list_of_files.append(file)
+        elif item_path.is_file():
+            list_of_files.append(File(item_path))
+    return list_of_files
+
+
+def save_index_to_json(path):
+    list_of_files = create_a_list_of_files(path)
+    with open("/home/zgodek/pipr/antivirus/index.json", 'w') as file_handle:
+        data = {}
+        for file in list_of_files:
+            file_data = {
+                "status": str(file.status()),
+                "hash_md5": str(file.hash_md5()),
+                "hash_sh1": str(file.hash_sh1())
+            }
+            data[str(file.path())] = file_data
+        json.dump(data, file_handle)
