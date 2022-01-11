17032d8bc065572cad458fe010e2edbd2458b49f
zgodek <zuzanna.godek@outlook.com>
2022-01-11 11:12:06 +0100

    Moved functions hash_md5 and hash_sha1 into File class.

--- file.py
+++ file.py
@@ -4,39 +4,35 @@ import hashlib
 class File:
     def __init__(self, path):
         self._path = path
-        self._hash_md5 = hash_a_file_md5(self.path())
-        self._hash_sh1 = hash_a_file_sh1(self.path())
+        self._status = False
 
     def path(self):
         return self._path
 
-    # def status(self):
-    #     return check_file(self.path())
+    def set_status(self, status):
+        self._status = status
+
+    def status(self):
+        return self._status
 
     def hash_md5(self):
-        return self._hash_md5
+        path = self._path
+        BLOCKSIZE = 65536
+        hasher = hashlib.sha1()
+        with open(path, 'rb') as file_handle:
+            buf = file_handle.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = file_handle.read(BLOCKSIZE)
+        return hasher.hexdigest()
 
     def hash_sh1(self):
-        return self._hash_sh1
-
-
-def hash_a_file_sh1(path):
-    BLOCKSIZE = 65536
-    hasher = hashlib.sha1()
-    with open(path, 'rb') as file_handle:
-        buf = file_handle.read(BLOCKSIZE)
-        while len(buf) > 0:
-            hasher.update(buf)
+        path = self._path
+        BLOCKSIZE = 65536
+        hasher = hashlib.md5()
+        with open(path, 'rb') as file_handle:
             buf = file_handle.read(BLOCKSIZE)
-    return hasher.hexdigest()
-
-
-def hash_a_file_md5(path):
-    BLOCKSIZE = 65536
-    hasher = hashlib.md5()
-    with open(path, 'rb') as file_handle:
-        buf = file_handle.read(BLOCKSIZE)
-        while len(buf) > 0:
-            hasher.update(buf)
-            buf = file_handle.read(BLOCKSIZE)
-    return hasher.hexdigest()
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = file_handle.read(BLOCKSIZE)
+        return hasher.hexdigest()
