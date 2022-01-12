47380ed3ec4a64ce2b868b9a4b41bd3b0f79231d
zgodek ubuntu <zuzanna.godek@outlook.com>
2022-01-12 23:23:49 +0100

    never mind

--- database.py
+++ database.py
@@ -1,76 +1,46 @@
 from pathlib import Path
-from file import File
-from database import Database
-import os
+import json
 
 
-database = Database()
+class Database:
+    def __init__(self):
+        self._dict_of_paths = {
+            "virus_hashes_path": "/home/zgodek/pipr/antivirus/database_md5",
+            "virus_sequences_path": "/home/zgodek/pipr/antivirus/database_sequences",
+            "index_path": "/home/zgodek/pipr/antivirus/index.json",
+            "test_directory": "/home/zgodek/pipr/antivirus/test_dir"
+        }
 
+    def change_path(self, which_path, new_path):
+        self._dict_of_paths[which_path] = new_path
 
-def full_scan(path):
-    path = Path(path)
-    files_with_viruses = []
-    for item_path in path.iterdir():
-        if item_path == Path(database.get_path("virus_sequences_path")) or item_path == Path(database.get_path("virus_hashes_path")):
-            continue
-        if item_path.is_dir():
-            viruses_in_folder = full_scan(item_path)
-            files_with_viruses.extend(viruses_in_folder)
-        if item_path.is_file():
-            if check_file(item_path):
-                files_with_viruses.append(item_path)
-    return files_with_viruses
+    def get_path(self, which_path):
+        return self._dict_of_paths[which_path]
 
+    def read_virus_database_md5(self):
+        path = self.get_path("virus_hashes_path")
+        path_database = Path(path)
+        list_of_hashes = []
+        for item_path in path_database.iterdir():
+            with open(item_path, 'r') as file_handle:
+                for line in file_handle:
+                    list_of_hashes.append(str(line).rstrip())
+        return list_of_hashes
 
-def quick_scan(path, dict_of_hashes=None):
-    if dict_of_hashes is None:
-        dict_of_hashes = database.read_index_database()
-    path = Path(path)
-    files_with_viruses = []
-    for item_path in path.iterdir():
-        if item_path == Path(database.get_path("virus_sequences_path")) or item_path == Path(database.get_path("virus_hashes_path")):
-            continue
-        if item_path.is_dir():
-            viruses_in_folder = quick_scan(item_path, dict_of_hashes)
-            if viruses_in_folder != []:
-                files_with_viruses.extend(viruses_in_folder)
-        else:
-            file = File(item_path)
-            if file.hash_sh1() not in dict_of_hashes.values():
-                if check_file(item_path):
-                    files_with_viruses.append(item_path)
-    return files_with_viruses
+    def virus_sequences_databse(self):
+        path = self.get_path("virus_sequences_path")
+        path_database = Path(path)
+        list_of_sequences = []
+        for item_path in path_database.iterdir():
+            with open(item_path, 'rb') as file_handle:
+                list_of_sequences.append(file_handle.read())
+        return list_of_sequences
 
-
-def check_file(path):
-    infected = False
-    file = File(path)
-    virus_hashes_md5 = database.read_virus_database_md5()
-    virus_sequences = database.virus_sequences_databse()
-    for virus_hash_md5 in virus_hashes_md5:
-        if virus_hash_md5 == file.hash_md5():
-            remove_file(path)
-            infected = True
-    with open(file.path(), 'rb') as file_handle:
-        byte_sequence = file_handle.read()
-        for virus_sequence in virus_sequences:
-            position = byte_sequence.find(virus_sequence)
-            if position >= 0:
-                fix_file(path, position)
-                infected = True
-    return infected
-
-
-def remove_file(path):
-    pass
-    # os.remove(path)
-
-
-def fix_file(path, position):
-    pass
-    # with open(path, 'wb') as file_handle:
-    #     pass
-
-
-def when_to_scan(time):
-    pass
+    def read_index_database(self):
+        path = self.get_path("index_path")
+        dict_of_hashes = {}
+        with open(path, 'r') as file_handle:
+            data = json.load(file_handle)
+            for item in data:
+                dict_of_hashes[str(item)] = data[str(item)]["hash_sh1"]
+        return dict_of_hashes
