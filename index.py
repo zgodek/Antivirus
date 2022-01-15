83cafde76ebf22f9d1aaaffbc95882cc08377c65
zgodek ubuntu <zuzanna.godek@outlook.com>
2022-01-15 23:21:46 +0100

    Created function update_index.

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import json
 from file import File
+import os
+import time
 
 
 def create_a_list_of_files(path):
@@ -15,9 +17,10 @@ def create_a_list_of_files(path):
     return list_of_files
 
 
-def save_index_to_json(path, database):
-    list_of_files = create_a_list_of_files(path)
-    index_path = database.get_path('index_path')
+def create_index(path, database, list_of_files=None):
+    if list_of_files == None:
+        list_of_files = create_a_list_of_files(path)
+    index_path = database.get_path('index_path') + f'/{os.path.split(path)[1]}_index'
     with open(index_path, 'w') as file_handle:
         data = {}
         for file in list_of_files:
@@ -25,11 +28,33 @@ def save_index_to_json(path, database):
                 "status": str(file.status()),
                 "hash_md5": str(file.hash_md5()),
                 "hash_sh1": str(file.hash_sh1()),
-                "update_time": str()
+                "last_updated": time.time()
             }
             data[str(file.path())] = file_data
         json.dump(data, file_handle)
 
 
-def update_index_in_json(filepath, database):
-    index_path = database.get_path('index_path')
+def when_files_were_last_updated(path, database, dict_of_old_files=None):
+    if dict_of_old_files == None:
+        dict_of_old_files = database.read_index_database(path)
+    for item_path in Path(path).iterdir():
+        if item_path.is_file():
+            item_path = item_path.as_posix()
+            if item_path in dict_of_old_files.keys():
+                last_time_updated = dict_of_old_files[item_path]["last_updated"]
+                if time.time() - last_time_updated < 3600:
+                    dict_of_old_files[item_path] = File(item_path)
+            else:
+                dict_of_old_files[item_path] = File(item_path)
+        else:
+            dict_of_old_files = when_files_were_last_updated(item_path, database, dict_of_old_files)
+    return dict_of_old_files
+
+
+def update_index(path, database):
+    list_of_updated_files = []
+    dict_of_updated_files = when_files_were_last_updated(path, database)
+    for item in dict_of_updated_files:
+        if os.path.isfile(Path(item)):
+            list_of_updated_files.append(dict_of_updated_files[item])
+    create_index(path, database, list_of_updated_files)
