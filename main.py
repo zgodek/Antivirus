b7c2c61eec578ef58e95572c1a4c3b18185f5b9c
zgodek ubuntu <zuzanna.godek@outlook.com>
2022-01-15 23:23:19 +0100

    Added print to full_scan and quick_scan so they list all of the found viruses.

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python
 from database import Database
 from scan import full_scan, quick_scan
-from index import save_index_to_json, update_index_in_json
-import sys
+from index import create_index, update_index
 import argparse
 
 
 database = Database()
 
-parser = argparse.ArgumentParser(description='Perform a full scan, a quick scan, create an index of a folder or update an index')
+parser = argparse.ArgumentParser(description='Perform a full scan, a quick scan, create an index of a folder or update an index of a folder')
 parser.add_argument('action', type=str, help='What type of action do you want to perform')
 parser.add_argument('path', type=str, help='Where do you want to perform chosen action')
-parser.add_argument('create index', type=str, help='Of which folder do you want to create an index')
-parser.add_argument('update index', type=str, help='Which folders index do you want to update')
 args = parser.parse_args()
 
 
 def main(action, path):
     if action == 'full scan':
-        full_scan(path, database)
+        print(full_scan(path, database))
     if action == 'quick scan':
-        quick_scan(path, database)
+        print(quick_scan(path, database))
     if action == 'create index':
-        save_index_to_json(path, database)
+        create_index(path, database)
     if action == 'update index':
-        update_index_in_json(path, database)
+        update_index(path, database)
     return
 
 
