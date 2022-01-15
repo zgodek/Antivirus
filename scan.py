c5d75590a93c6bad78a53ac11806e677c0338a46
zgodek ubuntu <zuzanna.godek@outlook.com>
2022-01-15 23:20:36 +0100

    Fixed quick_scan - added dict_of_files and an actual dict_of_hashes, fixed fix_file and it works properly.

--- scan.py
+++ scan.py
@@ -1,5 +1,11 @@
 from pathlib import Path
+from database import Database
 from file import File
+
+
+class IndexDoesntExistError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
 
 
 def full_scan(path, database_fs):
@@ -13,15 +19,19 @@ def full_scan(path, database_fs):
             files_with_viruses.extend(viruses_in_folder)
         if item_path.is_file():
             if check_file(item_path, database_fs):
-                files_with_viruses.append(item_path)
+                files_with_viruses.append(item_path.as_posix())
     return files_with_viruses
 
 
 def quick_scan(path, database_qs, dict_of_hashes=None):
     if dict_of_hashes is None:
-        dict_of_hashes = database_qs.read_index_database()
-        if dict_of_hashes == -1:
-            pass#jakis Error
+        try:
+            dict_of_hashes = {}
+            dict_of_files = database_qs.read_index_database(path)
+            for file in dict_of_files:
+                dict_of_hashes[file] = dict_of_files[file]["hash_sh1"]
+        except:
+            raise IndexDoesntExistError("A quick scan cannot be performed without an existing index for this path, please create an index first")
     path = Path(path)
     files_with_viruses = []
     for item_path in path.iterdir():
@@ -35,7 +45,7 @@ def quick_scan(path, database_qs, dict_of_hashes=None):
             file = File(item_path)
             if file.hash_sh1() not in dict_of_hashes.values():
                 if check_file(item_path, database_qs):
-                    files_with_viruses.append(item_path)
+                    files_with_viruses.append(item_path.as_posix())
     return files_with_viruses
 
 
@@ -52,6 +62,7 @@ def check_file(path, database_chf):
         for virus_sequence in virus_sequences:
             position = byte_sequence.find(virus_sequence)
             if position >= 0:
+                fix_file(file.path(), virus_sequence)
                 infected = True
     return infected
 
@@ -61,12 +72,9 @@ def remove_file(path):
     # os.remove(path)
 
 
-def fix_file(path, virus_sequence):
+def fix_file(path, virus_sequence: bytes):
     with open(path, 'rb') as file_handle:
-        file_code_string = str(file_handle.read())
-        file_code_string.replace(str(virus_sequence), "")
+        file_code = file_handle.read()
+        file_code = file_code.replace(virus_sequence, b"")
     with open(path, 'wb') as file_handle:
-        file_handle.write(bytes(file_code_string, "utf-8"))
-
-
-# fix_file("/home/zgodek/project_pipr/antivirus/susfile.txt", "aaa")
+        file_handle.write(file_code)
