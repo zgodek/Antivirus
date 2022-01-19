b03085de947711d4b25fcee4ada354fa3ccf75a7
zgodek ubuntu <zuzanna.godek@outlook.com>
2022-01-19 14:56:34 +0100

    Fixed checking database paths.

--- index.py
+++ index.py
@@ -1,17 +1,18 @@
-from asyncore import write
 from pathlib import Path
 import json
 from file import File
 import os
 
 
-def create_dict_of_files(path, dict_of_files=None):
+def create_dict_of_files(path, database, dict_of_files=None):
     if dict_of_files is None:
         dict_of_files = {}
     path = Path(path)
     for item_path in path.iterdir():
+        if item_path in database.get_paths():
+            continue
         if item_path.is_dir():
-            dict_of_files = create_dict_of_files(item_path, dict_of_files)
+            dict_of_files = create_dict_of_files(item_path, database, dict_of_files)
         elif item_path.is_file():
             item_path = item_path.as_posix()
             file = File(item_path)
@@ -32,7 +33,7 @@ def write_dict_to_index(path, database, dict_of_files):
 
 
 def create_index(path, database):
-    dict_of_files = create_dict_of_files(path)
+    dict_of_files = create_dict_of_files(path, database)
     write_dict_to_index(path, database, dict_of_files)
 
 
@@ -40,10 +41,12 @@ def when_files_were_last_updated(path, database, dict_of_old_files=None):
     if dict_of_old_files is None:
         dict_of_old_files = database.read_index_database(path)
     for item_path in Path(path).iterdir():
+        if item_path in database.get_paths():
+            continue
         if item_path.is_file():
             item_path = item_path.as_posix()
             file = File(item_path)
-            if item_path not in dict_of_old_files.keys():
+            if item_path in dict_of_old_files.keys():
                 last_time_scanned = dict_of_old_files[item_path]["last_scanned"]
                 if last_time_scanned is None or (os.path.getmtime(item_path)
                                                       > float(last_time_scanned)):
