df6542e864676daeec1fba944cf67efe1e7dc581
zgodek ubuntu <zuzanna.godek@outlook.com>
2022-01-20 10:49:45 +0100

    Added function that reads config file.

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from database import Database
 from scan import full_scan, quick_scan
 from index import create_index, update_index, write_dict_to_index
 import argparse
+import json
+import os
 
 
-database = Database()
+def read_config_paths():
+    dict_of_paths = {}
+    config_database_path = str(os.path.dirname(__file__)) + "/config_database.json"
+    with open(config_database_path, 'r') as file_handle:
+        data = json.load(file_handle)
+        for item in data:
+            dict_of_paths[item] = data[item]
+    return dict_of_paths
+
 
 parser = argparse.ArgumentParser(description='Perform a full scan, a quick scan, create an index of a folder or update an index of a folder')
 parser.add_argument('action', type=str, help='What type of action do you want to perform')
@@ -13,6 +23,7 @@ args = parser.parse_args()
 
 
 def main(action, path):
+    database = Database(read_config_paths())
     if action == 'full scan':
         full_scan_result = full_scan(path, database)
         print(full_scan_result[0])
