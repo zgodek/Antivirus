274e41879426e24dd5ab4845ab21ca18af3d38c0
zgodek ubuntu <zuzanna.godek@outlook.com>
2022-01-22 11:22:25 +0100

    Added function hash_md5_bytes which hashes only the byte sequence without reading it from a file.

--- file.py
+++ file.py
@@ -36,3 +36,17 @@ class File:
                 hasher.update(buf)
                 buf = file_handle.read(BLOCKSIZE)
         return hasher.hexdigest()
+
+
+def hash_md5_bytes(code_bytes: bytes):
+    BLOCKSIZE = 65536
+    hasher = hashlib.md5()
+    buf = code_bytes[0:BLOCKSIZE]
+    if buf == code_bytes:
+        hasher.update(code_bytes)
+    else:
+        while buf != "":
+            hasher.update(buf)
+            buf = code_bytes[BLOCKSIZE:BLOCKSIZE+BLOCKSIZE]
+            BLOCKSIZE += BLOCKSIZE
+    return hasher.hexdigest()
