ec47914ba2e7837fe93b493529773792abaaacbf
zgodek ubuntu <zuzanna.godek@outlook.com>
2022-01-22 18:52:41 +0100

    Changed with, removed tabs.

--- test_database.py
+++ test_database.py
@@ -31,50 +31,50 @@ def test_database_get_paths():
 
 
 def test_database_read_virus_hashes():
-    with tempfile.TemporaryDirectory() as tmpdirname_hashes:
-        with tempfile.NamedTemporaryFile(suffix=".txt", prefix="list_of_viruses", mode="w", dir=tmpdirname_hashes) as file_handle:
-            dict_of_paths = {
+    with tempfile.TemporaryDirectory() as tmpdirname_hashes, \
+            tempfile.NamedTemporaryFile(suffix=".txt", prefix="list_of_viruses", mode="w", dir=tmpdirname_hashes) as file_handle:
+        dict_of_paths = {
                 "virus_hashes_path": tmpdirname_hashes,
             }
-            file_handle.write("781770fda3bd3236d0ab8274577bbbbe\n247438632580f9c018c4d8f8d9c6c408")
-            file_handle.seek(0)
-            database = Database(dict_of_paths)
-            assert database.read_virus_database_md5() == ["781770fda3bd3236d0ab8274577bbbbe", "247438632580f9c018c4d8f8d9c6c408"]
+        file_handle.write("781770fda3bd3236d0ab8274577bbbbe\n247438632580f9c018c4d8f8d9c6c408")
+        file_handle.seek(0)
+        database = Database(dict_of_paths)
+        assert database.read_virus_database_md5() == ["781770fda3bd3236d0ab8274577bbbbe", "247438632580f9c018c4d8f8d9c6c408"]
 
 
 def test_database_read_virus_sequences():
-    with tempfile.TemporaryDirectory() as tmpdir_sequences:
+    with tempfile.TemporaryDirectory() as tmpdir_sequences, \
+            tempfile.NamedTemporaryFile(suffix=".txt", prefix="seq1", dir=tmpdir_sequences) as file_handle1, \
+            tempfile.NamedTemporaryFile(suffix=".txt", prefix="seq2", dir=tmpdir_sequences) as file_handle2:
         dict_of_paths = {
             "virus_sequences_path": tmpdir_sequences
         }
         database = Database(dict_of_paths)
-        with tempfile.NamedTemporaryFile(suffix=".txt", prefix="seq1", dir=tmpdir_sequences) as file_handle1:
-            with tempfile.NamedTemporaryFile(suffix=".txt", prefix="seq2", dir=tmpdir_sequences) as file_handle2:
-                file_handle1.write(b"12345")
-                file_handle1.seek(0)
-                file_handle2.write(b"$#@#@#")
-                file_handle2.seek(0)
-                assert b"12345" and b"$#@#@#" in database.read_virus_sequences_database()
+        file_handle1.write(b"12345")
+        file_handle1.seek(0)
+        file_handle2.write(b"$#@#@#")
+        file_handle2.seek(0)
+        assert b"12345" and b"$#@#@#" in database.read_virus_sequences_database()
 
 
 def test_database_read_index():
-    with tempfile.TemporaryDirectory() as tmpdirindex:
+    with tempfile.TemporaryDirectory() as tmpdirindex, \
+            tempfile.NamedTemporaryFile(mode="w+", suffix="folder_index", dir=tmpdirindex) as file_handle:
         dict_of_paths = {
             "index_path": tmpdirindex
         }
         database = Database(dict_of_paths)
-        with tempfile.NamedTemporaryFile(mode="w+", suffix="folder_index", dir=tmpdirindex) as file_handle:
-            test_dict = {
-                'path1': {
-                    'key1': 'value1',
-                    'key2': 'value2'
-                },
-                'path2': {
-                    'key3': 'value3',
-                    'key4': 'value4'
-                    }
+        test_dict = {
+            'path1': {
+                'key1': 'value1',
+                'key2': 'value2'
+            },
+            'path2': {
+                'key3': 'value3',
+                'key4': 'value4'
                 }
-            json.dump(test_dict, file_handle)
-            file_handle.flush()
-            path = file_handle.name.replace(("_index"), "")
-            assert database.read_index_database(path) == test_dict
+            }
+        json.dump(test_dict, file_handle)
+        file_handle.flush()
+        path = file_handle.name.replace(("_index"), "")
+        assert database.read_index_database(path) == test_dict
