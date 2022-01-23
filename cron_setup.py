6609d79b612cf8650b3e62988144e043dad3fd0b
zgodek ubuntu <zuzanna.godek@outlook.com>
2022-01-23 20:20:35 +0100

    Changed disable_cron so it stops regular quick scan of a certain path.

--- cron_setup.py
+++ cron_setup.py
@@ -1,17 +1,18 @@
 from crontab import CronTab
-from database import Database
 import os
 
 
 def set_up_cron(path, how_many_minutes):
     cron = CronTab(user=True)
     folder = os.path.dirname(__file__)
-    job = cron.new(command=f"python3 {folder}/main.py 'quick scan' '{path}' >> /home/zgodek/antivirus.log 2>&1")
+    job = cron.new(command=f"python3 {folder}/main.py 'quick scan' '{path}'", comment=path)
     job.minute.every(how_many_minutes)
     cron.write()
 
 
-def disable_cron():
+def disable_cron(path):
     cron = CronTab(user=True)
-    cron.remove_all()
-    cron.write()
+    for job in cron:
+        if job.comment == path:
+            cron.remove(job)
+            cron.write()
